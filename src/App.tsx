eccff8d5d976a56fc653afec3df717979f623f0e
Nurdanio <abdievv33@gmail.com>
2024-07-01 11:37:24 +0300

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from "react";
+
+export const App = () => {
+    const [count, setCount] = useState(0)
+
+    return <div>Salamat!</div>
+}
+
+
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { App } from "./App";
+
+const container= document.getElementById('root');
+ReactDOM.createRoot(container!).render(<App />)
